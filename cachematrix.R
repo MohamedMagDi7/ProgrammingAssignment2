bf30dc692197dcefce504d94eea77f4a3557bbe2
Mohamed Magdi <moh@crossworkers.com>
2020-07-07 15:22:35 +0200

    adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,20 @@
 ## Here we have two functions which are responsible for getting,
 ## setting and calculating the inverse of a matrix
 
+
 ## makeCacheMatrix function is responsible for creating the cache objects
 ## setting and getting the original matrix.
 ## setting and getting the inverse matrix.
-
 makeCacheMatrix <- function(x = matrix()) {
-    inverse <- NULL
-    set <- function(y) {
+    inverse <- NULL ##Initialize inverse with null
+    set <- function(y) { ## Initialize setter function
       x <<- y
       inverse <<- NULL
     }
-    get <- function() x
-    setInverse <- function(inv) inverse <<- inv
-    getInverse <- function() inverse
-    list(set = set, get = get,
+    get <- function() x ## Initialize getter function
+    setInverse <- function(inv) inverse <<- inv ## Initialize set Inverse function
+    getInverse <- function() inverse## Initialize get inverse function
+    list(set = set, get = get,## return list of functions in the object
          setInverse = setInverse,
          getInverse = getInverse)
 }
@@ -23,15 +23,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve function is responsible for checking for the 
 ## invert of the input matrix in the cache if so it returns the invert 
 ## else it calculates the invert and saves is to the cache
-
 cacheSolve <- function(x, ...) {
-  inverse <- x$getInverse()
-  if(!is.null(inverse)) {
+  inverse <- x$getInverse() ## get cached inverse
+  if(!is.null(inverse)) {## check if there is a cached inverse
     message("getting cached inverse matrix")
-    return(inverse)
+    return(inverse) ## return cached inverse if exist
   }
-  data <- x$get()
-  inverse <- solve(data)
-  x$setInverse(inverse)
-  inverse
+  data <- x$get()## get original matrix
+  inverse <- solve(data)## calculate inverse for the matrix
+  x$setInverse(inverse)## save inverse to cache
+  inverse ## return the calculated inverse
 }
